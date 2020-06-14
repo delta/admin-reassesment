f5b34490b42d9a5e15ecef1ef0a028876dda0c84
nimishagarwal76 <nimishagarwal76@gmail.com>
2020-06-14 15:44:59 +0530

    Change PORT

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ app.use('/auth', authenticateUser);
 //   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 // }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
